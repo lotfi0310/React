6b30db6f8aba54f572572f0914c71e51d35cec5e
lotfi0310 <lotfi0310@gmail.com>
2023-02-12 17:21:22 +0100

    increment number of like

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,53 @@
 
 import React, { Component } from 'react';
-import {Card,Button} from 'react-bootstrap';
-
-
-
+import {Button, Card, Container} from 'react-bootstrap';
+import {Row,Col} from 'react-bootstrap'
  export default class Product extends Component {
-
+ constructor(props){
+  super();
+  this.state ={
+     likes :0
+  };
+ }
     render() {
-
-
       return (
+<> 
+       <Card >
 
-        <Card>
-            <Card.Img
-            src={require('../assets/images/'+this.props.produit.img)}/>
-        </Card>
-    )
+              <Card.Body >
+              <Card.Img src={require('../assets/images/'+this.props.produit.img)}/>
+                <Card.Title>{this.props.produit.name}</Card.Title>
+                <Card.Text>
+                  { this.props.produit.description}
+                  <br/>
+                  <br/>
+                  {this.props.produit.price+'dt'}
+                </Card.Text>
+              </Card.Body>
+           <Container >
+            <Row>
+              <Col> 
+               <Button  style={{width:'20%' ,display:'flex' ,justifyContent:'space-between',marginLeft:'35%',flexDirection:'row',marginBottom:'3%'}}
+                onClick ={
+                  ()=>{
+                    this.setState({ likes: this.state.likes+1 });
+                    }} >
+                Like {this.state.likes}
+              </Button>
+              </Col>
+              <Col>
+              <Button style={{backgroundColor: '#00FFFF',width:'20%' ,display:'flex' ,justifyContent:'space-between',flexDirection:'row',marginBottom:'3%'}}>Buy</Button>
+              </Col>
+            </Row>
+           </Container>
+            
+             
+       </Card>
+    
+</>   
+        
+          
+        
+    );
 }
   }
--- src/components/Products.js
+++ src/components/Products.js
@@ -2,19 +2,32 @@ import React from 'react';
 import data from '../products.json';
 import Product from './Product';
 
+import { Container, Row , Col } from 'react-bootstrap';
 
 
 export class Products extends React.Component {
     render() {
        return(
+        
 
-<>
-
-{data.map((prod,index)=>(
-  <Product
+<> 
+<Container className='products'>
+  <Row>
+    <Col>
+    {data.map((prod,index)=>(
+    <Product
   key={index}
   produit={prod}/>
-))}
+  ))}
+    </Col>
+  </Row>
+</Container>
+
+ 
+   
+   
+
+
 </>);
          }
     
